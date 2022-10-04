7500b5dc5626f5a7bce2d6aa04ac02bc1c1fafba
Fatih <fatih@fatih.com>
2022-10-04 18:52:09 +0300

    Veri Erişim Katmanları Entity ve NHibernate

---
+++ NTAC.Core/DataAccess/IQueryableRepository.cs
@@ -0,0 +1,16 @@
+﻿using NTAC.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NTAC.Core.DataAccess
+{
+    public interface IQueryableRepository<T> where T : class, IEntity , new()
+    {
+        IQueryable<T> Table { get; } // Read Only
+
+
+    }
+}
